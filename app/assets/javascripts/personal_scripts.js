ff6ac2025579281d036445c0dc256e7e19d1f41b
Groovej <groovejbogdan@yandex.ru>
2013-03-13 20:16:19 +0200

    Change the draggable parametrs

--- app/assets/javascripts/personal_scripts.js
+++ app/assets/javascripts/personal_scripts.js
@@ -14,27 +14,21 @@ $(document).ready(function(){
 		  })
 	})
 	
+
+	
 	// drag the stickers
-	$( ".sticker" ).draggable({ 
-        disabled: true,
-        containment: "#main_container",
-	    scroll: false,
-	    cursor: "move",
-	    cursorAt: { top: 120, left: 80 }, 
-	    distance: 20,
-	    helper: 'original'	
-	});
+   
 	
 	/* configurations of block to_do to cach the stickers */
     $(".block_to_do").sortable({
-        revert: true,
-        connectWith: ["div.block_in_progress", "div.block_done"]
-    })
+        revert: 10,
+        connectWith: ["div.block_in_progress", "div.block_done"],
+      })
 
      
   $( ".block_to_do" ).droppable({
       disable: true,
-      accept: ".sticker",
+      accept: "div[class*='color']",
       drop: function( event, ui ) {
         $( this )
       }        
@@ -42,13 +36,13 @@ $(document).ready(function(){
     
     /* configurations of block in progress to cach the stickers */
    $(".block_in_progress").sortable({
-       revert: true,
+       revert: 10,
        connectWith: ["div.block_to_do", "div.block_done"] 
    })
    
    $( ".block_in_progress" ).droppable({
       disable: true,
-      accept: ".sticker",
+      accept: "div[class*='color']",
       drop: function( event, ui ) {
         $( this )
           }        
@@ -56,18 +50,33 @@ $(document).ready(function(){
     
      /* configurations of block done to cach the stickers */
    $(".block_done").sortable({
-       revert: true,
+       revert: 10,
        connectWith: ["div.block_to_do", "div.block_in_progress"]
    })
    
    $( ".block_done" ).droppable({
       disable: true,
-      accept: ".sticker",
+      accept: "div[class*='color']",
       drop: function( event, ui ) {
         $( this )
           }        
     });
  
+    
+     $('.sticker').draggable({
+         disabled: false
+     })
+     
+     $( "div[class~='color']" ).draggable({ 
+        disabled: true,
+        containment: "#main_container",
+        scroll: false,
+        cursor: "move",
+        cursorAt: { top: 120, left: 80 }, 
+        distance: 20,
+        helper: 'original',
+        
+    });
  
  
  /* ending tag*/     
