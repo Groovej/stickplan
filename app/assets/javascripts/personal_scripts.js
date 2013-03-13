22219cdefde2a9c52d8d8644f71643ffef4f711d
Groovej <groovejbogdan@yandex.ru>
2013-03-13 14:20:41 +0200

    Change dropable parameters for blocks

--- app/assets/javascripts/personal_scripts.js
+++ app/assets/javascripts/personal_scripts.js
@@ -12,30 +12,55 @@ $(document).ready(function(){
 	$( ".sticker" ).draggable({ 
         disabled: true,
         containment: "#main_container",
-	    scroll: false, cursor: "move",
+	    scroll: false,
+	    cursor: "move",
 	    cursorAt: { top: 120, left: 80 }, 
 	    distance: 20,
-	    connectToSortable: ".block_to_do",
-        helper: 'original'	
+	    helper: 'original'	
 	});
 	
-	$(".sticker" ).draggable({ 
-	      cancel: "p#text" 
-	    })
-      
-    
+	/* configurations of block to_do to cach the stickers */
     $(".block_to_do").sortable({
-        revert: true
-        })
+        revert: true,
+        connectWith: ["div.block_in_progress", "div.block_done"]
+    })
 
      
   $( ".block_to_do" ).droppable({
+      disable: true,
       accept: ".sticker",
       drop: function( event, ui ) {
         $( this )
           }        
     });
     
+    /* configurations of block in progress to cach the stickers */
+   $(".block_in_progress").sortable({
+       revert: true,
+       connectWith: ["div.block_to_do", "div.block_done"] 
+   })
+   
+   $( ".block_in_progress" ).droppable({
+      disable: true,
+      accept: ".sticker",
+      drop: function( event, ui ) {
+        $( this )
+          }        
+    });
+    
+     /* configurations of block done to cach the stickers */
+   $(".block_done").sortable({
+       revert: true,
+       connectWith: ["div.block_to_do", "div.block_in_progress"]
+   })
+   
+   $( ".block_done" ).droppable({
+      disable: true,
+      accept: ".sticker",
+      drop: function( event, ui ) {
+        $( this )
+          }        
+    });
    
     
    
