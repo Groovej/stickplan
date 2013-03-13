3c26f9b5345ccbb8a43db379e5558538a62be023
Groovej <groovejbogdan@yandex.ru>
2013-03-13 13:35:26 +0200

    Creating drag and drop functionality

--- app/assets/javascripts/personal_scripts.js
+++ app/assets/javascripts/personal_scripts.js
@@ -9,43 +9,34 @@ $(document).ready(function(){
 	})
 	
 	// drag the stickers
-	$( ".sticker" ).draggable({ containment: "#main_container", scroll: false, cursor: "move", cursorAt: { top: 120, left: 80 }, distance: 20 });
+	$( ".sticker" ).draggable({ 
+        disabled: true,
+        containment: "#main_container",
+	    scroll: false, cursor: "move",
+	    cursorAt: { top: 120, left: 80 }, 
+	    distance: 20,
+	    connectToSortable: ".block_to_do",
+        helper: 'original'	
+	});
+	
+	$(".sticker" ).draggable({ 
+	      cancel: "p#text" 
+	    })
       
     
     $(".block_to_do").sortable({
         revert: true
         })
-   
-   $( ".sticker" ).draggable({
-      connectToSortable: ".block_to_do",
-      snap: true,
-      snapMode: "both",
-      revert: false
-      });
-      
-      
-    $(".block_to_do").disableSelection();
+
      
   $( ".block_to_do" ).droppable({
+      accept: ".sticker",
       drop: function( event, ui ) {
         $( this )
-          
-          }
+          }        
     });
     
-   $( ".block_in_progress" ).droppable({
-      drop: function( event, ui ) {
-        $( this )
-          
-          }
-    });
-    
-    $( ".block_done" ).droppable({
-      drop: function( event, ui ) {
-        $( this )
-          
-          }
-    });
+   
     
    
     
